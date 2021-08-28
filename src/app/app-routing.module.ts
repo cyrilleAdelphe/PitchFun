529d5bed7bbee49f44b438d8bfe4b1b1d235fa2c
unknown <madazada0@gmail.com>
2021-08-28 02:22:53 +0200

    add home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent/* ,
+    children: [
+      {
+        path: '',
+        component: HomePageComponent,
+        outlet: 'child'
+      }
+    ] */
+  },
+  {
+    path: 'test',
+    component: HomePageComponent
+  }  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
